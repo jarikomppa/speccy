7f9a8222acb7d80a89833709903acadb38b21faa
jarikomppa <jari.komppa@gmail.com>
2015-09-30 09:26:33 +0300

    (asm optimized) text output, some other little things

--- app/app.c
+++ app/app.c
@@ -7,6 +7,8 @@ unsigned char sin_idx;
 #include "tab.h"
 #include "s.h"
 const unsigned char *bufp;
+unsigned char *data_ptr;
+unsigned char *screen_ptr;
 
 // xxxsmbbb
 // where b = border color, m is mic, s is speaker
@@ -20,6 +22,7 @@ void port254(const unsigned char color) __z88dk_fastcall
     __endasm;    
 }
 
+// practically waits for retrace
 void do_halt()
 {
     __asm
@@ -27,6 +30,95 @@ void do_halt()
     __endasm;
 }
 
+// draw a single 8x8 character (x in 8 pixel steps, y in 1 pixel steps)
+void drawchar(unsigned char c, unsigned char x, unsigned char y)
+{
+    data_ptr = (unsigned char*)(15616-32*8+c*8);
+    screen_ptr = (unsigned char*)yofs[y]+x;
+    __asm
+        ld bc,(_screen_ptr)
+        ld hl,(_data_ptr)
+        ld a,(hl)
+    	ld (bc),a
+    	inc hl
+    	inc b
+        ld a,(hl)
+    	ld (bc),a
+    	inc hl
+    	inc b
+        ld a,(hl)
+    	ld (bc),a
+    	inc hl
+    	inc b
+        ld a,(hl)
+    	ld (bc),a
+    	inc hl
+    	inc b
+        ld a,(hl)
+    	ld (bc),a
+    	inc hl
+    	inc b
+        ld a,(hl)
+    	ld (bc),a
+    	inc hl
+    	inc b
+        ld a,(hl)
+    	ld (bc),a
+    	inc hl
+    	inc b
+        ld a,(hl)
+    	ld (bc),a
+    __endasm;
+}
+
+// draw string, x in 8 pixel steps, y in 1 pixel steps
+void drawstring(unsigned char *t, unsigned char x, unsigned char y)
+{
+    screen_ptr = (unsigned char*)yofs[y]+x;
+    while (*t)
+    {
+        data_ptr = (unsigned char*)(15616-32*8+*t*8);
+        __asm
+            ld bc,(_screen_ptr)
+	        ld hl,(_data_ptr)
+	        ld a,(hl)
+        	ld (bc),a
+        	inc hl
+        	inc b
+	        ld a,(hl)
+        	ld (bc),a
+        	inc hl
+        	inc b
+	        ld a,(hl)
+        	ld (bc),a
+        	inc hl
+        	inc b
+	        ld a,(hl)
+        	ld (bc),a
+        	inc hl
+        	inc b
+	        ld a,(hl)
+        	ld (bc),a
+        	inc hl
+        	inc b
+	        ld a,(hl)
+        	ld (bc),a
+        	inc hl
+        	inc b
+	        ld a,(hl)
+        	ld (bc),a
+        	inc hl
+        	inc b
+	        ld a,(hl)
+        	ld (bc),a
+        __endasm;
+        x++;        
+        screen_ptr++;
+        t++;
+    }
+}
+
+// copy N scanlines from linear memory to video memory
 void fbcopy(const unsigned char * src, unsigned char start, unsigned char end)
 {
     for (fbcopy_idx = start; fbcopy_idx < end; fbcopy_idx++, src+=32)
@@ -35,19 +127,33 @@ void fbcopy(const unsigned char * src, unsigned char start, unsigned char end)
     }
 }
 
+// experimental: make some noise
+void playtone(unsigned char delay)
+{
+    unsigned char d = delay;
+    port254(0xff);
+    while (d--);
+    d = delay;
+    port254(0);
+    while (d--);
+}
+
 void main()
 {       
     sin_idx = 0;
-    
     while(1)
     {
         sin_idx++;
         bufp = s_png;
         bufp += sinofs[sin_idx];
         do_halt(); // halt waits for interrupt - or vertical retrace
-        // can do about 64 scanlines in a frame
-        port254(1);
-        fbcopy(bufp, 64, 120);
-        port254(0);
+    // delay loop to move the border into the frame (for profiling)
+    for (fbcopy_idx = 0; fbcopy_idx < 145; fbcopy_idx++) port254(0);
+    port254(2);
+    drawstring("Hello Worldie!", 0, 8);
+    port254(1);
+    // can do about 64 scanlines in a frame (with nothing else)
+    fbcopy(bufp, 64, 110);
+    port254(0);
     }    
 }
