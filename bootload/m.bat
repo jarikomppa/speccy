8e2075b11b80c66e4713398717a4cb8ee44b1b4f
jarikomppa <jari.komppa@gmail.com>
2015-10-06 10:10:14 +0300

    added no-clear flag

--- bootload/m.bat
+++ bootload/m.bat
@@ -3,6 +3,6 @@ sdld -i -b _HEADER=52945 boot.rel
 ..\tools\ihx2bin boot.ihx boot.bin
 ..\tools\lzfpack ..\app\app.bin app.bin.lzf
 copy /b boot.bin+app.bin.lzf combined.bin
-..\tools\bootpack combined.bin
+..\tools\bootpack combined.bin -nc
 call call_appmake.bat
 speccy combined.tap
