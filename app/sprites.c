91d6a4156ca0e6f2306de7f64a5bdba44155c62f
jarikomppa <jari.komppa@gmail.com>
2015-11-27 13:41:09 +0200

    using stack to load sprite data yields some speedup

--- app/sprites.c
+++ app/sprites.c
@@ -54,7 +54,7 @@ void drawsprite(char *aData, short aX, short aY)
 */
 
 
-
+/*
 void drawsprite(char *aData, short aX, short aY)
 {    
     aData; aX; aY;
@@ -112,9 +112,7 @@ void drawsprite(char *aData, short aX, short aY)
         ex de,hl
         push de
         pop bc
-        
-
-        
+              
 #define HPASS  \
         ld a, (de)  \
         and (hl)    \
@@ -235,7 +233,187 @@ void drawsprite(char *aData, short aX, short aY)
         add hl, bc
         ex de,hl
         HPASS
+#undef HPASS        
+        pop de
+        pop bc
+        pop hl
+        pop iy
+        pop ix            
+    __endasm;   
+}
+*/
+
+void drawsprite(char *aData, short aX, short aY)
+{    
+    aData; aX; aY;
+     
+    __asm
+        push ix
+    	ld	ix,#0
+    	add	ix,sp
+    
+        push iy
+        push hl
+        push bc
+        push de
         
+        ld a, 6 (ix) ; aX
+        and a, #7
+        ld c, a
+        ld b, #0
+        
+        ; now we need to multiply bc by 16*6
+        push bc
+        pop hl     ; hl = 1*bc       
+        add hl, bc ; hl = 2*bc
+        add hl, bc ; hl = 3*bc
+        add hl, hl ; hl = 6*bc
+        add hl, hl ; hl = 2*6*bc
+        add hl, hl ; hl = 4*6*bc
+        add hl, hl ; hl = 8*6*bc
+        add hl, hl ; hl = 16*6*bc        
+        push hl
+        pop bc
+
+        ld d, 9 (ix) ; aY
+        ld e, 8 (ix) 
+        ld hl, #(_yofs)
+        add hl, de      
+        add hl, de      
+        push hl
+        pop iy
+        
+        ld h, 5 (ix) ; aData
+        ld l, 4 (ix)
+        add hl, bc ; inside-byte shift added
+        
+        ex de,hl
+        ld h, 7 (ix) ; aX
+        ld l, 6 (ix)
+        // divide aX by 8
+        srl h
+        rr l
+        srl h
+        rr l
+        srl h
+        rr l
+        ex de,hl
+        push de
+        pop bc
+        ld sp, hl
+              
+#define HPASS       \
+        pop hl      \
+        ld a, (de)  \
+        and l       \
+        or h        \
+        ld (de),a   \
+        inc de      \
+                    \
+        pop hl      \
+        ld a, (de)  \
+        and l       \
+        or h        \
+        ld (de),a   \
+        inc de      \
+                    \
+        pop hl      \
+        ld a, (de)  \
+        and l       \
+        or h        \
+        ld (de),a   
+
+        ld l, 0 (iy)
+        ld h, 1 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 2 (iy)
+        ld h, 3 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 4 (iy)
+        ld h, 5 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 6 (iy)
+        ld h, 7 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 8 (iy)
+        ld h, 9 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 10 (iy)
+        ld h, 11 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 12 (iy)
+        ld h, 13 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 14 (iy)
+        ld h, 15 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 16 (iy)
+        ld h, 17 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 18 (iy)
+        ld h, 19 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 20 (iy)
+        ld h, 21 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 22 (iy)
+        ld h, 23 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 24 (iy)
+        ld h, 25 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 26 (iy)
+        ld h, 27 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 28 (iy)
+        ld h, 29 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+        ld l, 30 (iy)
+        ld h, 31 (iy)
+        add hl, bc
+        ex de,hl
+        HPASS
+#undef HPASS
+        ld sp, ix
+        dec sp
+        dec sp
+        dec sp
+        dec sp
+        dec sp
+        dec sp
+        dec sp
+        dec sp
+
         pop de
         pop bc
         pop hl
@@ -260,6 +438,13 @@ void clearsprite(short aX, short aY)
     }    
 }
 
+#include "..\tools\bubble.c"
+/*
+void drawsprite(char *aData, short aX, short aY)
+{
+    drawsprite_bubble(aX, aY);
+}
+*/
 
 //unsigned char oldx, oldy;
 
@@ -272,6 +457,66 @@ void spritetest()
     //clearsprite(127-oldx, 127-oldy);
     //oldx = x;    oldy = y;
     x = 0; y = 0;
+/*    drawsprite_bubble(0,0);
+    drawsprite_bubble(1,0);
+    drawsprite_bubble(2,0);
+    drawsprite_bubble(3,0);
+    drawsprite_bubble(4,0);
+
+    drawsprite_bubble(5,0);
+    drawsprite_bubble(6,0);
+    drawsprite_bubble(7,0);
+    drawsprite_bubble(0,0);
+    drawsprite_bubble(1,0);
+
+
+    drawsprite_bubble(2,0);
+    drawsprite_bubble(3,0);
+    drawsprite_bubble(4,0);
+    drawsprite_bubble(5,0);
+    drawsprite_bubble(6,0);
+
+    drawsprite_bubble(7,0);
+    drawsprite_bubble(0,0);
+    drawsprite_bubble(1,0);
+    drawsprite_bubble(2,0);
+    drawsprite_bubble(3,0);
+
+
+    drawsprite_bubble(4,0);
+    drawsprite_bubble(5,0);
+    drawsprite_bubble(6,0);
+    drawsprite_bubble(7,0);
+    drawsprite_bubble(0,0);
+
+    drawsprite_bubble(1,0);
+    */
+/*    
+    drawsprite(bubble, 0, 0);    
+    drawsprite(bubble, 16, 0);    
+    drawsprite(bubble, 32, 0);    
+    drawsprite(bubble, 48, 0);    
+    drawsprite(bubble, 64, 0);    
+
+    drawsprite(bubble, 80, 0);    
+    drawsprite(bubble, 96, 0);    
+    drawsprite(bubble, 112, 0);    
+    drawsprite(bubble, 128, 0);    
+    drawsprite(bubble, 144, 0);    
+
+    drawsprite(bubble, 160, 0);    
+    drawsprite(bubble, 176, 0);    
+    drawsprite(bubble, 192, 0);    
+    drawsprite(bubble, 208, 0);    
+    drawsprite(bubble, 224, 0);    
+
+    drawsprite(bubble, 240, 0);    
+    drawsprite(bubble, 0, 16);    
+    drawsprite(bubble, 16, 16);    
+    drawsprite(bubble, 32, 16);    
+    drawsprite(bubble, 48, 16);    
+*/
+
     
     drawsprite(bubble, (sinofs[(framecounter + framecounter+x) & 0xff] & 255) + 16, (sinofs[(framecounter+y) & 0xff] & 127)+24); x += 10; y -= 10; port254(2);    
     drawsprite(bubble, (sinofs[(framecounter + framecounter+x) & 0xff] & 255) + 16, (sinofs[(framecounter+y) & 0xff] & 127)+24); x += 10; y -= 10; port254(0);
@@ -292,4 +537,5 @@ void spritetest()
     drawsprite(bubble, (sinofs[(framecounter + framecounter+x) & 0xff] & 255) + 16, (sinofs[(framecounter+y) & 0xff] & 127)+24); x += 10; y -= 10;port254(0);
     drawsprite(bubble, (sinofs[(framecounter + framecounter+x) & 0xff] & 255) + 16, (sinofs[(framecounter+y) & 0xff] & 127)+24); x += 10; y -= 10;port254(2);
     drawsprite(bubble, (sinofs[(framecounter + framecounter+x) & 0xff] & 255) + 16, (sinofs[(framecounter+y) & 0xff] & 127)+24);port254(0);
+    
 }
