ab6cb7367e4e9d80168f0ce552f2b4f0ff3a6ea5
jarikomppa <jari.komppa@gmail.com>
2015-12-29 14:10:28 +0200

    chopped 5 bytes..

--- loadscrn/boot.s
+++ loadscrn/boot.s
@@ -11,12 +11,12 @@ bootloader:
     push hl
     ld hl, #1300 ; compressed data len (needs patching)
     push hl
-    ld hl, #24097 ; source position (needs patching?) 23806+269
+    ld hl, #24092 ; source position (needs patching?) 23828+264
     push hl
     push hl ; needs another push to make the counts match
  	push ix
  	
-	ld	ix,#0
+	ld	ix,#10
 	add	ix,sp
 	ld	hl,#-6
 	add	hl,sp
@@ -24,12 +24,12 @@ bootloader:
 	ld	bc,#0 ;idx
 next_data:
 	ld	a,c
-	sub	a, 6 (ix) ; len
+	sub	a, -4 (ix) ; len
 	ld	a,b
-	sbc	a, 7 (ix) ; len
+	sbc	a, -3 (ix) ; len
 	jr	NC, done_island
-	ld	l,4 (ix) ; src
-	ld	h,5 (ix) ; src
+	ld	l,-6 (ix) ; src
+	ld	h,-5 (ix) ; src
 	add	hl,bc
 	ld	h,(hl)
 	ld	a,h
@@ -37,32 +37,32 @@ next_data:
 	rlca
 	rlca
 	and	a,#0x07
-	ld	-2 (ix),a     ; run len
-	ld	-1 (ix),#0x00 ; run len
-	ld	l,-2 (ix)
+	ld	-12 (ix),a     ; run len
+	ld	-11 (ix),#0x00 ; run len
+	ld	l,-12 (ix)
 	inc	bc            ; idx
-	ld	-4 (ix),c     ; idx
-	ld	-3 (ix),b     ; idx
-	ld	a,4 (ix)      ; src
-	add	a, -4 (ix)    ; idx
+	ld	-14 (ix),c     ; idx
+	ld	-13 (ix),b     ; idx
+	ld	a,-6 (ix)      ; src
+	add	a, -14 (ix)    ; idx
 	ld	c,a
-	ld	a,5 (ix)      ; src
-	adc	a, -3 (ix)    ; idx
+	ld	a,-5 (ix)      ; src
+	adc	a, -13 (ix)    ; idx
 	ld	b,a
 	ld	a,h
 	and	a, #0x1F
 	ld	e,a
 	ld	d,#0x00
-	ld	a,-1 (ix) ; run len
-	or	a,-2 (ix) ; run len
+	ld	a,-11 (ix) ; run len
+	or	a,-12 (ix) ; run len
 	jr	NZ, not_literal
 	inc	de
 	; prep for memcpy
 	push	de
 	push	bc
 	push	de
-	ld	l,8 (ix) ; dst
-	ld	h,9 (ix) ; dst
+	ld	l,-2 (ix) ; dst
+	ld	h,-1 (ix) ; dst
 	push	hl
 	pop de
 	pop bc
@@ -70,16 +70,16 @@ next_data:
 	ldir
 	; / memcpy
 	pop	de
-	ld	a,8 (ix) ; dst
+	ld	a,-2 (ix) ; dst
 	add	a, e
-	ld	8 (ix),a
-	ld	a,9 (ix) ; dst
+	ld	-2 (ix),a
+	ld	a,-1 (ix) ; dst
 	adc	a, d
-	ld	9 (ix),a  ; dst
-	ld	a,-4 (ix) ; idx
+	ld	-1 (ix),a  ; dst
+	ld	a,-14 (ix) ; idx
 	add	a, e
 	ld	c,a
-	ld	a,-3 (ix) ; idx
+	ld	a,-13 (ix) ; idx
 	adc	a, d
 	ld	b,a
 next_data_island:
@@ -97,37 +97,37 @@ not_literal:
 	ld	h,a
 	ld	a,d
 	or	a, b
-	ld	-6 (ix), h
-	ld	-5 (ix), a
-	ld	c,-4 (ix)
-	ld	b,-3 (ix)
+	ld	-16 (ix), h
+	ld	-15 (ix), a
+	ld	c,-14 (ix)
+	ld	b,-13 (ix)
 	inc	bc
-	ld	a,-2 (ix)
+	ld	a,-12 (ix)
 	sub	a, #0x07
 	jr	NZ,do_run
-	ld	a,-1 (ix)
+	ld	a,-11 (ix)
 	or	a, a
 	jr	NZ,do_run
-	ld	l,4 (ix) ; src
-	ld	h,5 (ix) ; src
+	ld	l,-6 (ix) ; src
+	ld	h,-5 (ix) ; src
 	add	hl,bc
 	ld	e,(hl)
 	ld	d,#0x00
 	ld	hl,#0x0007
 	add	hl,de
-	ld	-2 (ix),l
-	ld	-1 (ix),h
+	ld	-12 (ix),l
+	ld	-11 (ix),h
 	inc	bc
 do_run:
-	ld	e,-2 (ix)
-	ld	d,-1 (ix)
+	ld	e,-12 (ix)
+	ld	d,-11 (ix)
 	inc	de
 	inc	de
-	ld	a,8 (ix) ; dst
-	sub	a, -6 (ix)
+	ld	a,-2 (ix) ; dst
+	sub	a, -16 (ix)
 	ld	l,a
-	ld	a,9 (ix) ; dst
-	sbc	a, -5 (ix)
+	ld	a,-1 (ix) ; dst
+	sbc	a, -15 (ix)
 	ld	h,a
 	dec	hl
 	; memcpy prep
@@ -135,8 +135,8 @@ do_run:
 	push	de
 	push	hl
 	push	de
-	ld	l,8 (ix) ; dst
-	ld	h,9 (ix) ; dst
+	ld	l,-2 (ix) ; dst
+	ld	h,-1 (ix) ; dst
 	push	hl
 	pop de
 	pop bc
@@ -145,19 +145,14 @@ do_run:
 	; / memcpy
 	pop	de
 	pop	bc
-	ld	a,8 (ix) ; dst
+	ld	a,-2 (ix) ; dst
 	add	a, e
-	ld	8 (ix),a
-	ld	a,9 (ix) ; dst
+	ld	-2 (ix),a
+	ld	a,-1 (ix) ; dst
 	adc	a, d
-	ld	9 (ix),a ; dst
+	ld	-1 (ix),a ; dst
 	jr	next_data_island
 done:
 	ld sp, ix
-	pop hl
-	pop hl
-	pop hl
-	pop hl
-	pop hl
 	ei
     ret
