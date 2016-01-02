af0a0e7e0c9bad70b73fff048f6a77b4e66ee29b
jarikomppa <jari.komppa@gmail.com>
2016-01-02 17:54:23 +0200

    instead of hardcoded filename, use parameter (still far from robust)

--- tools/png2bin.cpp
+++ tools/png2bin.cpp
@@ -12,7 +12,7 @@
 int main(int parc, char ** pars)
 {
     int x, y, comp;
-    stbi_uc * raw = stbi_load("s.png", &x, &y, &comp, 4);
+    stbi_uc * raw = stbi_load(pars[1], &x, &y, &comp, 4);
     printf("%d %d %d\n", x, y, comp);
     unsigned int *p = (unsigned int *)raw;
     FILE * f = fopen("png.bin","wb");
@@ -22,7 +22,7 @@ int main(int parc, char ** pars)
     for (i = 0; i < x*y; i++)
     {
         d <<= 1;
-        d |= (p[i] & 0xffffff) == 0;
+        d |= p[i] == p[0];
         if (i % 8 == 7)
         {
             fputc(d, f);
