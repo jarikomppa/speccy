cce950643a40c305f2ac08668550c93b18fd505b
jarikomppa <jari.komppa@gmail.com>
2016-06-11 08:46:20 +0300

    All game modes done. Just missing audio now. And possible bug fixes. And tweaking. I guess.

--- quiz/app.c
+++ quiz/app.c
@@ -16,6 +16,8 @@ unsigned char *screen_ptr;
 
 unsigned short framecounter;
 
+unsigned short score[4];
+
 #define HWIF_IMPLEMENTATION
 
 #include "yofstab.h"
@@ -227,28 +229,29 @@ void clearfields(char q, char a)
         st((unsigned char*)yofs[ 1 * 8 + i] + 15, 0, 16);
 
     if (a)
-    for (i = 0; i < 16; i++)
     {
-        st((unsigned char*)yofs[16 * 8 + i] +  3, 0, 12);
-        st((unsigned char*)yofs[16 * 8 + i] + 17, 0, 12);
-        st((unsigned char*)yofs[20 * 8 + i] +  3, 0, 12);
-        st((unsigned char*)yofs[20 * 8 + i] + 17, 0, 12);
+        for (i = 0; i < 16; i++)
+        {
+            st((unsigned char*)yofs[16 * 8 + i] +  3, 0, 12);
+            st((unsigned char*)yofs[16 * 8 + i] + 17, 0, 12);
+            st((unsigned char*)yofs[20 * 8 + i] +  3, 0, 12);
+            st((unsigned char*)yofs[20 * 8 + i] + 17, 0, 12);
+        }
+        st((unsigned char*)0x5800+16*32+3,7,12);
+        st((unsigned char*)0x5800+17*32+3,7,12);
+    
+        st((unsigned char*)0x5800+20*32+3,7,12);
+        st((unsigned char*)0x5800+21*32+3,7,12);
+    
+        st((unsigned char*)0x5800+16*32+17,7,12);
+        st((unsigned char*)0x5800+17*32+17,7,12);
+    
+        st((unsigned char*)0x5800+20*32+17,7,12);
+        st((unsigned char*)0x5800+21*32+17,7,12);
     }
-    st((unsigned char*)0x5800+16*32+3,7,12);
-    st((unsigned char*)0x5800+17*32+3,7,12);
-
-    st((unsigned char*)0x5800+20*32+3,7,12);
-    st((unsigned char*)0x5800+21*32+3,7,12);
-
-    st((unsigned char*)0x5800+16*32+17,7,12);
-    st((unsigned char*)0x5800+17*32+17,7,12);
-
-    st((unsigned char*)0x5800+20*32+17,7,12);
-    st((unsigned char*)0x5800+21*32+17,7,12);
-
 }
 
-void setplayerbright(unsigned char player, unsigned char bright)
+void setplayerbright(unsigned char player, char bright)
 {
     unsigned char xofs, v;
     switch (player)
@@ -275,6 +278,8 @@ void setplayerbright(unsigned char player, unsigned char bright)
     {
         v |= COLOR(0,1,0,0);
     }
+    if (bright < 0)
+        v = 0;
     // y = 11
     st((unsigned char*)0x5800+11*32+xofs,v,3);
     st((unsigned char*)0x5800+12*32+xofs,v,5);
@@ -283,7 +288,7 @@ void setplayerbright(unsigned char player, unsigned char bright)
     st((unsigned char*)0x5800+14*32+xofs,v,5);
 }
 
-void setanswerbright(unsigned char answer, unsigned char bright)
+void setanswerbright(unsigned char answer, char bright)
 {
     unsigned char xofs, yofs, v;
     switch (answer)
@@ -314,6 +319,8 @@ void setanswerbright(unsigned char answer, unsigned char bright)
     {
         v |= COLOR(0,1,0,0);
     }
+    if (bright < 0)
+        v = 0;
     st((unsigned char*)0x5800+yofs*32+xofs,v,14);
     *((unsigned char*)0x5820+yofs*32+xofs) = v;
     *((unsigned char*)0x582d+yofs*32+xofs) = v;
@@ -465,10 +472,6 @@ void mainmenu()
     unsigned short robotalk = 0;
     unsigned char blurbno = 0;
     clearfields(1,1);
-    drawstringfancy("000", 6, 13, 7, 255);
-    drawstringfancy("000", 12, 13, 7, 255);
-    drawstringfancy("000", 18, 13, 7, 255);
-    drawstringfancy("000", 24, 13, 7, 255);
     setanswerbright(0, 0);
     setanswerbright(1, 0);
     setanswerbright(2, 0);
@@ -604,24 +607,96 @@ void mainmenu()
         robotalk++;
         xorshift16();        
     }    
+    score[0] = 0;
+    score[1] = 0;
+    score[2] = 0;
+    score[3] = 0;
+}
+
+void printscore(char player)
+{
+    char temp[4];
+    char xofs;
+    unsigned short sum = score[player];
+    temp[0] = '0';
+    temp[1] = '0';
+    temp[2] = '0';
+    temp[3] = 0;
+    switch (player)
+    {
+    default:
+    case 0:
+        xofs = 6;
+        break;
+    case 1:
+        xofs = 12;
+        break;
+    case 2:
+        xofs = 18;
+        break;
+    case 3:
+        xofs = 24;
+        break;
+    }
+    while (sum >= 100 && temp[0] != '9')
+    {
+        temp[0]++;
+        sum -= 100;
+    }
+    while (sum >= 100)
+    {
+        sum -= 100;
+    }
+    while (sum >= 10)
+    {
+        temp[1]++;
+        sum -= 10;
+    }
+    while (sum >= 1)
+    {
+        temp[2]++;
+        sum -= 1;
+    }
+    
+    drawstringfancy(temp, xofs, 13, 7, 255);
 }
 
 void ingame()
 {
     char done = 0;
+    char currentplayer = 0;
     unsigned short turn = 0;
     unsigned short maxturn = 0;
-    unsigned short robotalk;
+    unsigned short robotalk = 0;
     unsigned char keydown = 0;
     char i;
     for (i = 0; i < players; i++)
         maxturn += 12;
     
+    printscore(0);
+    printscore(1);
+    printscore(2);
+    printscore(3);
+    
     while (!done)
     {
         char *a[4];
         char correct;
+        char what;
+        char who;
         unsigned short q;
+        what = 0;
+        who = 0;
+        
+        if (gamemode != 4)
+        {
+            setplayerbright(0, 0);
+            setplayerbright(1, 0);
+            setplayerbright(2, 0);
+            setplayerbright(3, 0);
+            setplayerbright(currentplayer, 1);
+        }
+            
 
         clearfields(1,1);
         q = xorshift16() & 255;
@@ -668,22 +743,199 @@ void ingame()
             do_halt();
             do_halt();
             do_halt();
-            if (KEYDOWN(1))
-                keydown = 1;
+            if (gamemode == 4)
+            {
+                switch (players)
+                {
+                    default:
+                    case 1:
+                        print_keys(0,0);
+                        break;
+                    case 2:
+                        print_keys(0,1);
+                        print_keys(1,0);
+                        break;
+                    case 3:
+                        print_keys(0,2);
+                        print_keys(1,1);
+                        print_keys(2,0);
+                        break;
+                    case 4:
+                        print_keys(0,3);
+                        print_keys(1,2);
+                        print_keys(2,1);
+                        print_keys(3,0);
+                        break;
+                }
+            }
+            else
+            {
+                print_keys(currentplayer, 0);
+            }
+            
+            if (gamemode < 3 || gamemode == 3 && currentplayer == 0 || gamemode == 4)
+            {
+                if (KEYDOWN(1))
+                {
+                    what = 0;
+                    who = 0;
+                    keydown = 1;
+                }
+                if (KEYDOWN(2))
+                {
+                    what = 1;
+                    who = 0;
+                    keydown = 1;
+                }
+                if (KEYDOWN(3))
+                {
+                    what = 2;
+                    who = 0;
+                    keydown = 1;
+                }
+                if (KEYDOWN(4))
+                {
+                    what = 3;
+                    who = 0;
+                    keydown = 1;
+                }
+            }
+            if (gamemode == 3 && currentplayer == 1 || gamemode == 4)
+            {
+                if (KEYDOWN(7))
+                {
+                    what = 0;
+                    who = 1;
+                    keydown = 1;
+                }
+                if (KEYDOWN(8))
+                {
+                    what = 1;
+                    who = 1;
+                    keydown = 1;
+                }
+                if (KEYDOWN(9))
+                {
+                    what = 2;
+                    who = 1;
+                    keydown = 1;
+                }
+                if (KEYDOWN(0))
+                {
+                    what = 3;
+                    who = 1;
+                    keydown = 1;
+                }
+            }       
+            if (gamemode == 3 && currentplayer == 2 || gamemode == 4)
+            {
+                if (KEYDOWN(Z))
+                {
+                    what = 0;
+                    who = 2;
+                    keydown = 1;
+                }
+                if (KEYDOWN(X))
+                {
+                    what = 1;
+                    who = 2;
+                    keydown = 1;
+                }
+                if (KEYDOWN(C))
+                {
+                    what = 2;
+                    who = 2;
+                    keydown = 1;
+                }
+                if (KEYDOWN(V))
+                {
+                    what = 3;
+                    who = 2;
+                    keydown = 1;
+                }
+            }       
+            if (gamemode == 3 && currentplayer == 3 || gamemode == 4)
+            {
+                if (KEYDOWN(H))
+                {
+                    what = 0;
+                    who = 3;
+                    keydown = 1;
+                }
+                if (KEYDOWN(J))
+                {
+                    what = 1;
+                    who = 3;
+                    keydown = 1;
+                }
+                if (KEYDOWN(K))
+                {
+                    what = 2;
+                    who = 3;
+                    keydown = 1;
+                }
+                if (KEYDOWN(L))
+                {
+                    what = 3;
+                    who = 3;
+                    keydown = 1;
+                }
+            }       
             robotalk++;
+            xorshift16();
         }
+        drawstringfancy(a[0]+1,3,16,7,*a[0]);
+        drawstringfancy(a[1]+1,17,16,7,*a[1]);
+        drawstringfancy(a[2]+1,3,20,7,*a[2]);
+        drawstringfancy(a[3]+1,17,20,7,*a[3]);
+        drawstringfancy(tempq+1,15,1,7,*tempq);
+        mouth(0x66);
+        eyes(0);
         
-        setanswerbright(correct, 1);
-        for (i = 0; i < 15; i++) do_halt();
-        setanswerbright(correct, 0);
-        for (i = 0; i < 15; i++) do_halt();
-        setanswerbright(correct, 1);
-        for (i = 0; i < 15; i++) do_halt();
-        setanswerbright(correct, 0);
-        for (i = 0; i < 15; i++) do_halt();
-        setanswerbright(correct, 1);
-        for (i = 0; i < 15; i++) do_halt();
-        setanswerbright(correct, 0);
+        if (what == correct)
+        {
+            score[who]++;
+            printscore(who);
+            setplayerbright(who, 1);       
+            setanswerbright(correct, 1);
+            for (i = 0; i < 15; i++) do_halt();
+            setplayerbright(who, 0);       
+            setanswerbright(correct, -1);
+            for (i = 0; i < 15; i++) do_halt();
+            setplayerbright(who, 1);       
+            setanswerbright(correct, 1);
+            for (i = 0; i < 15; i++) do_halt();
+            setplayerbright(who, 0);       
+            setanswerbright(correct, -1);
+            for (i = 0; i < 15; i++) do_halt();
+            setplayerbright(who, 1);       
+            setanswerbright(correct, 1);
+            for (i = 0; i < 15; i++) do_halt();
+            setplayerbright(who, 0);       
+            setanswerbright(correct, 0);
+            if (gamemode < 2)
+                setplayerbright(who, 1);       
+        }
+        else
+        {
+            if (gamemode == 4 && score[who] > 0)
+                score[who]--;
+            printscore(who);
+            setplayerbright(who, 1);       
+            setanswerbright(correct, 1);
+            for (i = 0; i < 15; i++) do_halt();
+            setanswerbright(correct, 1);
+            for (i = 0; i < 15; i++) do_halt();
+            setanswerbright(correct, -1);
+            for (i = 0; i < 15; i++) do_halt();
+            setanswerbright(correct, 1);
+            for (i = 0; i < 15; i++) do_halt();
+            setanswerbright(correct, 1);
+            for (i = 0; i < 15; i++) do_halt();
+            setanswerbright(correct, 0);
+            if (gamemode > 1)
+                setplayerbright(who, 0);       
+        }
         
         while (ANYKEY())
         {
@@ -694,8 +946,12 @@ void ingame()
         
         keydown = 0;
         
+        currentplayer++;
+        if (currentplayer == players)
+            currentplayer = 0;
+        
         turn++;
-        if (turn == maxturn)
+        if (turn == maxturn && gamemode != 2)
             done = 1;
     }
     clearfields(1,1);
@@ -745,6 +1001,11 @@ void main()
     get_question(50);
         
     cp((unsigned char*)0x4000, 6912, (char*)0x5b00);
+
+    drawstringfancy("000", 6, 13, 7, 255);
+    drawstringfancy("000", 12, 13, 7, 255);
+    drawstringfancy("000", 18, 13, 7, 255);
+    drawstringfancy("000", 24, 13, 7, 255);
                
     y8 = 1;
     y16 = 1;
