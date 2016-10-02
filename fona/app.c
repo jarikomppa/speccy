7cf66204c98b1e3a401d84fa728de60bd801cde2
jarikomppa <jari.komppa@gmail.com>
2016-10-02 13:00:33 +0300

    left to right printer, slightly slower than the top-down one, but more psychologically pleasing for some reason.

--- fona/app.c
+++ fona/app.c
@@ -26,7 +26,7 @@ unsigned short framecounter = 0;
 void drawstring(unsigned char *aS, unsigned char aX, unsigned char aY)
 {
     unsigned char i, *s, *d, sx, x;
-    unsigned char *datap = (unsigned char*)(int*)test_data - 32*FONTHEIGHT;
+    unsigned char *datap = (unsigned char*)(int*)test_data - 32 * FONTHEIGHT;
     unsigned char *widthp = (unsigned char*)(int*)test_width - 32;
     for (i = 0; i < FONTHEIGHT; i++)
     {
@@ -55,6 +55,42 @@ void drawstring(unsigned char *aS, unsigned char aX, unsigned char aY)
     }
 }
 
+void drawstring_lr(unsigned char *s, unsigned char x, unsigned char y)
+{
+    unsigned char i;
+    char sx = 0;
+    unsigned char *yp[8];
+    unsigned char *datap = (unsigned char*)(int*)test_data - 32 * FONTHEIGHT;
+    unsigned char *widthp = (unsigned char*)(int*)test_width - 32;
+    for (i = 0; i < 8; i++)
+    {
+        yp[i] = (unsigned char*)yofs[y + i] + x;
+    }
+    
+    while (*s)
+    {
+        unsigned char ch = *s;
+        unsigned char wd = widthp[ch];
+        unsigned char *chp = datap + ch * FONTHEIGHT;
+        for (i = 0; i < 8; i++, chp++)
+        {
+            *(yp[i]) |= *chp >> sx;                        
+        }
+        sx += wd;
+        if (sx > 8)
+        {
+            chp -= 8;
+            sx -= 8;
+            for (i = 0; i < 8; i++, chp++)
+            {
+                yp[i]++;
+                *(yp[i]) |= *chp << (wd - sx);
+            }
+        }
+        s++;
+    }
+}
+
 void main()
 {
     unsigned short i;
@@ -71,29 +107,29 @@ void main()
     
     while(1)
     {
-        /*
-        for (i = 0; i < 192*32; i++)
-            *(unsigned char*)(0x4000+i) = 0;
-*/
+        
+
         framecounter++;
         do_halt(); // halt waits for interrupt - or vertical retrace
         for (i = 0; i < 600; i++);
 
         port254(1);
-        drawstring("Test", 0, 0);
-/*                           
-        drawstring("Venison frankfurter turducken boudin. Filet", 0, 0);
-        drawstring("mignon meatloaf ham hamburger, pork chop ", 0, 8*1);
-        drawstring("drumstick kevin prosciutto t-bone chuck short ", 0, 8*2);
-        drawstring("loin. Tenderloin burgdoggen bacon porchetta", 0, 8*3);
-        drawstring("pork chop leberkas. Filet mignon pork", 0, 8*4);
-        drawstring("andouille flank, shankle brisket turkey ", 0, 8*5);
-        drawstring("bacon bresaola. Prosciutto pork chop", 0, 8*6);
-        drawstring("hamburger pastrami strip steak pork belly pig ", 0, 8*7);
-        drawstring("frankfurter rump cupim biltong shoulder ", 0, 8*8);
-        drawstring("andouille chicken kevin. Pork belly ", 0, 8*9);
-        drawstring("meatloaf kielbasa tri-tip meatball.", 0, 8*10);
-        */
+//        drawstring_lr("Test", 0, 0);
+                           
+        for (i = 0; i < 192*32; i++)
+            *(unsigned char*)(0x4000+i) = 0;
+        drawstring_lr("Venison frankfurter turducken boudin. Filet", 0, 0);
+        drawstring_lr("mignon meatloaf ham hamburger, pork chop ", 0, 8*1);
+        drawstring_lr("drumstick kevin prosciutto t-bone chuck short ", 0, 8*2);
+        drawstring_lr("loin. Tenderloin burgdoggen bacon porchetta", 0, 8*3);
+        drawstring_lr("pork chop leberkas. Filet mignon pork", 0, 8*4);
+        drawstring_lr("andouille flank, shankle brisket turkey ", 0, 8*5);
+        drawstring_lr("bacon bresaola. Prosciutto pork chop", 0, 8*6);
+        drawstring_lr("hamburger pastrami strip steak pork belly pig ", 0, 8*7);
+        drawstring_lr("frankfurter rump cupim biltong shoulder ", 0, 8*8);
+        drawstring_lr("andouille chicken kevin. Pork belly ", 0, 8*9);
+        drawstring_lr("meatloaf kielbasa tri-tip meatball.", 0, 8*10);
+
         //drawstring_dumb("What a fixed world it is!", 0, 9);
         port254(0);
     }
