70cf908e99187970d9b7f95cde3d72c56c4bf106
jarikomppa <jari.komppa@gmail.com>
2018-12-06 13:29:22 +0200

    starting asm optimizations on the new drawstring

--- engarde/drawstring.s
+++ engarde/drawstring.s
@@ -1,195 +1,224 @@
 	.module drawstring
-	
 	.globl _drawstringz
 	.area _CODE
 
-;   9(ix) = aY
-;   8(ix) = aX
-;  -7(ix) = i (row)
-;   6(ix) &   7(ix) = aS
-;  -3(ix) &  -4(ix) = datap
-;  -1(ix) &  -2(ix) = dest pointer
-;  -5(ix) &  -6(ix) = dest base
-
+	
+	;           7(ix) = aY
+	;           6(ix) = aX
+	;   4(ix)   5(ix) = aS
+	;  -1(ix)  -2(ix) = bd
+	;  -3(ix)  -4(ix) = datap
+	;  -5(ix)  -6(ix) = s
+	;          -7(ix) = g
+	;          -8(ix) = i
+	;          -9(ix) = pixofs
+	; -10(ix) -11(ix) = d
+	;         -12(ix) = w
+	
+	
 ; void drawstringz(unsigned char *aS, unsigned char aX, unsigned char aY)
 _drawstringz::
-	push	ix
-	push iy
+	push ix
 	ld	ix,#0
 	add	ix,sp
-	ld	hl,#-15
+	ld	hl,#-13
 	add	hl,sp
 	ld	sp,hl
-; font data pointer (-32*8 because data starts from space)
-	ld	-4 (ix),#<((_propfont_data - 0x0100))
-	ld	-3 (ix),#>((_propfont_data - 0x0100))
-; Multiply input y by 8
-	ld	a,9 (ix)
-	rlca
-	rlca
-	rlca
-	and	a,#0xf8
-	ld	9 (ix),a
-	
-; row loop counter 
-	ld	-7 (ix),#0x08
-
-; calculate destination offset (from lookup table)
-	ld	l,9 (ix)
+                        ;drawstring.c:5: const unsigned char *datap = (unsigned char*)(propfont + 94 - 32); // font starts from space (32)
+	ld	-4 (ix),#<((_propfont + 0x003e))
+	ld	-3 (ix),#>((_propfont + 0x003e))
+                        ;drawstring.c:6: const unsigned char *widthp = (unsigned char*)(propfont - 32);
+                        ;drawstring.c:7: const unsigned char *shiftp = (unsigned char*)(propfont + 846);
+                        ;drawstring.c:8: unsigned char *bd = (unsigned char*)yofs[aY * 8] + aX;
+	ld	bc,#_yofs+0
+	ld	l,7 (ix)
 	ld	h,#0x00
 	add	hl, hl
-	ld	de,#_yofs
-	add	hl,de
-	ld	e,(hl)
+	add	hl, hl
+	add	hl, hl
+	add	hl, hl
+	add	hl,bc
+	ld	c,(hl)
 	inc	hl
 	ld	b,(hl)
-	ld	a,8 (ix)
-	add	a, e
-	ld	-6 (ix),a
-	ld	a,#0x00
-	adc	a, b
-	ld	-5 (ix),a
-
-; Row loop - do this 8 times (for 8 pixel high chars)
-rowloop:
-; set iy to point at input string
-	ld	c, 6 (ix)
-	ld	b, 7 (ix)
-	ld iy, #0x00
-	add iy, bc
-
-; destination byte can fit 8 bits
-	ld	c,#0x08
-; destination byte
-;	ld	b,#0x00 ; not actually needed 
-
-; set up destination pointer (base ptr increments per row, copy runs with string)
-	ld a, -5 (ix)
-	ld -1 (ix), a
-	ld a, -6 (ix)
-	ld -2 (ix), a	
-					
-; Loop while input char is not zero
-nextchar:
-; Grab char and take data slice.
-; This could be optimized by reorganizing data to be linear slice-wise,
-; instead of all 8 bytes of a glyph at once.
-	ld	l, (iy)
-	ld	h, #0x00
-	add	hl, hl
-	add	hl, hl
-	add	hl, hl
-	ld	e,-4 (ix)
-	ld	d,-3 (ix)
-	add	hl,de
-	ld	a, (hl)
-	ld  d, a
-	
-; Find glyph's width
-	ld	a, #<((_propfont_width - 0x0020))
-	add	a, (iy)
-	ld	l, a
-	ld	a, #>((_propfont_width - 0x0020))
+	ld	a,c
+	add	a, 6 (ix)
+	ld	-2 (ix),a
+	ld	a,b
 	adc	a, #0x00
-	ld	h, a
-	ld	a, (hl)
-	ld	e, a
-	
-;drawstring.c:22: while (width)
+	ld	-1 (ix),a
+                        ;drawstring.c:10: for (i = 0; i < 8; i++)
+	ld	-8 (ix),#0x08
+
+rowloop:
+                        ;drawstring.c:12: unsigned char *d = bd;
+	ld	a,-2 (ix)
+	ld	-11 (ix),a
+	ld	a,-1 (ix)
+	ld	-10 (ix),a
+                        ;drawstring.c:13: unsigned char *s = aS;        
+	ld	a,4 (ix)
+	ld	-6 (ix),a
+	ld	a,5 (ix)
+	ld	-5 (ix),a
+                        ;drawstring.c:19: unsigned char ch = *s;
+	ld	l,-6 (ix)
+	ld	h,-5 (ix)
+	ld	c,(hl)
+                        ;drawstring.c:20: unsigned char w = widthp[ch];
+	ld	hl,#(_propfont - 0x0020)
+	ld	b,#0x00
+	add	hl, bc
+	ld	a,(hl)
+	ld	-12 (ix),a
+                        ;drawstring.c:21: unsigned char g = datap[ch];
+	ld	l,-4 (ix)
+	ld	h,-3 (ix)
+	ld	b,#0x00
+	add	hl, bc
+	ld	a,(hl)
+	ld	-7 (ix),a
+                        ;drawstring.c:22: if (g)
+	or	a, a
+	jr	Z,fast_emptydata
+                        ;drawstring.c:24: unsigned short si = (unsigned short)g * 2 + pixofs * 2;
+	ld l,a
+	ld	h,#0x00
+	add	hl, hl
+	ex	de,hl
+                        ;drawstring.c:25: *d |= shiftp[si];
 	ld	l,-2 (ix)
 	ld	h,-1 (ix)
-
-; does this glyph fit completely in byte?
-	sub a, c
-	jr NC, prewidthloop   
-; pre-decrement target space
-	ld a, c
-	sub a, e
-	ld c, a   
-	ld a, d
-; Loop for glyph's width
-fastwidthloop:	
-	; Rotate pixel through carry to output byte
-	rla
-	rl b
-	
-	dec e
-	jr NZ, fastwidthloop	
-	jr chardone    
-
-prewidthloop:
-	ld a, d
-; Loop for glyph's width	
-widthloop:
-	
-	; Rotate pixel through carry to output byte
-	rla
-	rl b
-	
-; Decrement pixels that fit in target
-	dec	c
-	jr	NZ,bytefull
-; we're full, flush byte to screen
-	ld	c, #0x08
-	ld	(hl), b
-	inc	hl
-bytefull:
-; Decrement width, loop if not done with glyph
-	dec e
-	jr NZ, widthloop
-
-chardone:
-; store hl (dest ptr)
-	ld	-2 (ix),l
-	ld	-1 (ix),h
-
-; next char, check if we're at end of string
-	inc iy
-	ld a, (iy)
+	ld	c,(hl)
+	ld	hl,#(_propfont + 0x034e)
+	add	hl,de
+	ld	a,(hl)
+	or	a, c
+	ld	l,-2 (ix)
+	ld	h,-1 (ix)
+	ld	(hl),a
+                        ;drawstring.c:26: pixofs += w;
+fast_emptydata:
+	ld	a,-12 (ix)
+	ld	-9 (ix),a
+                        ;drawstring.c:32: s++;                    
+	ld	c,-6 (ix)
+	ld	b,-5 (ix)
+	inc	bc
+                        ;drawstring.c:35: while (*s)
+charloop:
+	ld	a,(bc)
+	ld	e,a
 	or	a, a
-	jr	NZ,nextchar
-lastchar:
-
-; do we have leftover data?
-	ld	a, c
-	sub	a, #0x08
-	jr	Z, evenbits
-; Do we have at least 5 pixels to go?
-	ld	a,c
-	sub	a, #0x05
-	jr	C, lastshifts
-; Do 4 pixels on one go
-	dec	c
-	dec	c
-	dec	c
-	dec	c
-	ld a,b
-	add a,a
-	add a,a
-	add a,a
-	add a,a
-	ld	b,a
-; Do the last pixels one at a time
-lastshifts:
-	sla	b
-	dec	c
-	jr	NZ,lastshifts
-; Put last byte on screen
-	ld	(hl),b
-evenbits:
-
-
-; Move destination base pointer down one line
-	inc	-5 (ix)
-; 16 bit inc of the data pointer to access the next layer    
-	inc	-4 (ix)	
-	jr	NZ,dataadd16
-	inc	-3 (ix)
-dataadd16:
-; Check if we've done all 8 lines
-	dec	-7 (ix)
-	jp NZ, rowloop
+	jp	Z,endofstring
+                        ;drawstring.c:37: unsigned char ch = *s;
+                        ;drawstring.c:38: unsigned char w = widthp[ch];
+	ld	hl,#(_propfont - 0x0020)
+	ld	d,#0x00
+	add	hl, de
+	ld	d,(hl)
+                        ;drawstring.c:39: unsigned char g = datap[ch];
+	ld	a,-4 (ix)
+	add	a, e
+	ld	l,a
+	ld	a,-3 (ix)
+	adc	a, #0x00
+	ld	h,a
+	ld	e,(hl)
+	ld	l,e
+                        ;drawstring.c:44: pixofs += w;
+	ld	a,-9 (ix)
+	add	a, d
+	ld	-7 (ix),a
+                        ;drawstring.c:40: if (g)
+	ld	a,e
+	or	a, a
+	jr	Z,emptydata
+                        ;drawstring.c:42: unsigned short si = (unsigned short)g * 2 + pixofs * 2;
+	ld	h,#0x00
+	add	hl, hl
+	ex	de,hl
+	ld	l,-9 (ix)
+	ld	h,#0x00
+	add	hl, hl
+	add	hl,de
+	ex	de,hl
+                        ;drawstring.c:43: *d |= shiftp[si];
+	ld	l,-11 (ix)
+	ld	h,-10 (ix)
+	ld	a,(hl)
+	ld	-6 (ix),a
+	ld	hl,#(_propfont + 0x034e)
+	add	hl,de
+	ld	a,(hl)
+	or	a, -6 (ix)
+	ld	l,-11 (ix)
+	ld	h,-10 (ix)
+	ld	(hl),a
+                        ;drawstring.c:44: pixofs += w;
+	ld	a,-7 (ix)
+	ld	-9 (ix),a
+                        ;drawstring.c:45: if (pixofs > 7)
+	ld	a,#0x07
+	sub	a, -9 (ix)
+	jr	NC,withinbyte
+                        ;drawstring.c:47: d++; 
+	inc	-11 (ix)
+	jr	NZ,add16bit
+	inc	-10 (ix)
+add16bit:
+                        ;drawstring.c:48: *d |= shiftp[si+1];
+	ld	l,-11 (ix)
+	ld	h,-10 (ix)
+	ld	a,(hl)
+	ld	-6 (ix),a
+	inc	de
+	ld	hl,#(_propfont + 0x034e)
+	add	hl,de
+	ld	a,(hl)
+	or	a, -6 (ix)
+	ld	l,-11 (ix)
+	ld	h,-10 (ix)
+	ld	(hl),a
+                        ;drawstring.c:49: pixofs &= 7;
+	ld	a,-9 (ix)
+	and	a, #0x07
+	ld	-9 (ix),a
+	jr	withinbyte
+emptydata:
+                        ;drawstring.c:54: pixofs += w;
+	ld	a,-7 (ix)
+	ld	-9 (ix),a
+                        ;drawstring.c:55: if (pixofs > 7)
+	ld	a,#0x07
+	sub	a, -9 (ix)
+	jr	NC,withinbyte
+                        ;drawstring.c:57: pixofs &= 7;
+	ld	a,-9 (ix)
+	and	a, #0x07
+	ld	-9 (ix),a
+                        ;drawstring.c:58: d++;
+	inc	-11 (ix)
+	jr	NZ,add16bit2
+	inc	-10 (ix)
+add16bit2:
+withinbyte:
+                        ;drawstring.c:61: s++;                    
+	inc	bc
+	jp	charloop
+endofstring:
+                        ;drawstring.c:63: datap += 94;
+	ld	a,-4 (ix)
+	add	a, #0x5e
+	ld	-4 (ix),a
+	ld	a,-3 (ix)
+	adc	a, #0x00
+	ld	-3 (ix),a
+                        ;drawstring.c:64: bd += 0x0100;
+	inc -1 (ix)
+                        ;drawstring.c:10: for (i = 0; i < 8; i++)
+	dec	-8 (ix)
+	jp	NZ,rowloop
 	ld	sp, ix
-	pop iy
 	pop	ix
 	ret
