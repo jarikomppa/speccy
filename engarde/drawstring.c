59f2df70babbd9b251a121a70cc37fca2f3b5e1e
jarikomppa <jari.komppa@gmail.com>
2018-12-06 12:30:32 +0200

    Bunch of optimizations

--- engarde/drawstring.c
+++ engarde/drawstring.c
@@ -12,195 +12,55 @@ void drawstringz(unsigned char *aS, unsigned char aX, unsigned char aY)
         unsigned char *d = bd;
         unsigned char *s = aS;        
         unsigned char pixofs = 0;
+
+		// first char special case
+		
+		{
+            unsigned char ch = *s;
+            unsigned char w = widthp[ch];
+            unsigned char g = datap[ch];
+			if (g)
+			{
+				unsigned short si = (unsigned short)g * 2 + pixofs * 2;
+				*d |= shiftp[si];
+				pixofs += w;
+			}
+			else
+			{
+				pixofs += w;
+			}
+            s++;                    
+		}
+		
         while (*s)
         {
             unsigned char ch = *s;
             unsigned char w = widthp[ch];
             unsigned char g = datap[ch];
-            unsigned short si = (unsigned short)g * 2 + pixofs * 2;
-            *d |= shiftp[si];
-            pixofs += w;
-            if (pixofs >= 8)
-            {
-                d++;
-            }
-            if (pixofs > 8)
-            {
-                *d |= shiftp[si+1];
-            }
-            pixofs &= 7;
+			if (g)
+			{
+				unsigned short si = (unsigned short)g * 2 + pixofs * 2;
+				*d |= shiftp[si];
+				pixofs += w;
+				if (pixofs > 7)
+				{
+					d++; 
+					*d |= shiftp[si+1];
+					pixofs &= 7;
+				}
+			}
+			else
+			{
+				pixofs += w;
+				if (pixofs > 7)
+				{
+					pixofs &= 7;
+					d++;
+				}
+			}
             s++;                    
         }
         datap += 94;
         bd += 0x0100;
     }
 }
-/*
-void drawstringz(unsigned char *aS, unsigned char aX, unsigned char aY)
-{
-    unsigned char i, *s, *d;
-    const unsigned char *datap = (unsigned char*)propfont_data - 32 * 8; // font starts from space (32)
-    const unsigned char *widthp = (unsigned char*)propfont_width - 32;
-    aY *= 8;
-    for (i = 0; i < 8; i++)
-    {
-		unsigned char ch = *aS;
-		unsigned char sx, pd;
-        s = aS;
-        sx = 0;
-		pd = 0;
-        d = (unsigned char*)yofs[aY] + aX;
-        while (ch)
-        {
-			unsigned char data = datap[ch * 8];
-			unsigned char width = widthp[ch];
-			
-			while (width)
-			{
-				pd <<= 1;
-				pd |= (data & 0x80) != 0;				
-				data <<= 1;
-
-				sx++;
-				if (sx == 8)
-				{
-					sx = 0;
-					*d = pd;
-					d++;
-				}
-				
-
-				width--;
-			}
-
-            s++;
-			ch = *s;
-        }
-		
-		if (sx)
-		{
-			if (sx < 4)
-			{
-				sx += 4;
-				pd <<= 4;
-			}
-			while (sx != 7)
-			{
-				pd <<= 1;
-				sx++;
-			}
-			*d = pd;
-		}
-		
-        aY++;
-        datap++;
-    }
-}
-
-
-
-// 282 bytes as of c7f5295
-void drawstring(unsigned char *aS, unsigned char aX, unsigned char aY)
-{
-    unsigned char i, *s, *d, sx, c;
-    unsigned char *datap = (unsigned char*)(int*)propfont_data - 32 * 8;
-    unsigned char *widthp = (unsigned char*)(int*)propfont_width - 32;
-    aY *= 8;
-    for (i = 0; i < 8; i++)
-    {
-        s = aS + 1;
-        c = *aS;
-        sx = 0;
-        d = (unsigned char*)yofs[aY] + aX;
-        while (c)
-        {
-            unsigned char ch = *s;
-            if (ch != 128)
-            {
-                unsigned char data = datap[ch * 8];
-                unsigned char width = widthp[ch];
-                if (data)
-                {
-                    *d |= data >> sx;
-                }
-                
-                sx += width;
-                if (sx > 8)
-                {
-                    d++;
-                    sx -= 8;
-                    if (data)
-                    {
-                        *d = data << (width - sx);
-                    }
-                }
-            }
-            s++;
-            c--;
-        }
-        aY++;
-        datap++;
-    }
-}
-*/
-
-/*
-// 410 bytes (as of c7f5295)
-void drawstringz(unsigned char *aS, unsigned char x, unsigned char y)
-{
-    unsigned char i;
-    unsigned char c;
-    char sx = 0;
-    unsigned char *yp[8];
-    unsigned char *datap = (unsigned char*)(int*)propfont_data - 32 * 8;
-    unsigned char *widthp = (unsigned char*)(int*)propfont_width - 32;
-    unsigned char *s = aS;
-    unsigned char len = 0;
-    while (aS[len]) len++;
-    
-    for (i = 0; i < 8; i++)
-    {
-        yp[i] = (unsigned char*)yofs[y + i] + x;
-    }
-    c = len;    
-    while (c)
-    {
-        unsigned char ch = *s;
-        unsigned char wd = widthp[ch];
-        unsigned char *chp = datap + ch * 8;
-        if (*s != 32)
-        {
-            for (i = 0; i < 8; i++, chp++)
-            {
-                unsigned char data = *chp;
-                if (data)
-                {
-                    *(yp[i]) |= data >> sx;                        
-                }
-            }
-        }
-        else
-        {
-            chp += 8;
-        }
-        
-        sx += wd;
-        if (sx > 8)
-        {
-            sx -= 8;
-            chp -= 8;            
-            for (i = 0; i < 8; i++, chp++)
-            {
-                unsigned char data = *chp;
-                yp[i]++;
-                if (data)
-                {
-                    *(yp[i]) = data << (wd - sx);
-                }
-            }            
-        }
-        s++;
-        c--;
-    }
-}
-*/
