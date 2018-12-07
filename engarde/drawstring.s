f0ec9d979bb53715898fe511228c3c67cc1f5165
Jari Komppa <jari.komppa@gmail.com>
2018-12-07 16:37:54 +0200

    mostly reformating, but changed and 0x07:s to res 3 to save maybe two clocks a glyph

--- engarde/drawstring.s
+++ engarde/drawstring.s
@@ -14,44 +14,44 @@
 	; -10(ix) -11(ix) = d			7     -> bc´
 	;         -12(ix) = w			2     -> juggled through b/c
 	; unused: iy
-		
+
 ; void drawstringz(unsigned char *aS, unsigned char aX, unsigned char aY)
 _drawstringz::
 	push ix
-	ld	ix,#0
-	add	ix,sp
+	ld	ix, #0
+	add	ix, sp
 
 	; move datap to de´ (glyph data indices)
 	exx
-	ld	e,#<((_propfont + 0x003e))
-	ld	d,#>((_propfont + 0x003e))
+	ld	e, #<((_propfont + 0x003e))
+	ld	d, #>((_propfont + 0x003e))
 	exx
-	
+
 	; calculate pointer to screen, yofs from table
-	ld	bc,#_yofs+0
-	ld	l,7 (ix)
-	ld	h,#0x00	
+	ld	bc, #_yofs+0
+	ld	l, 7 (ix)
+	ld	h, #0x00
 	add	hl, hl      ; aY * 8
 	add	hl, hl
 	add	hl, hl
 	add	hl, hl
-	add	hl,bc
-	ld	c,(hl)
+	add	hl, bc
+	ld	c, (hl)
 	inc	hl
-	ld	b,(hl)
-	
-	ld	a,c
+	ld	b, (hl)
+
+	ld	a, c
 	add	a, 6 (ix)   ; + aX
-	ld l, a
-	ld	a,b
+	ld  l, a
+	ld  a, b
 	adc	a, #0x00
-	ld h, a
-	
+	ld  h, a
+
 	; base target pointer calculated, store:
 	ld (drawstringz_local_bd + 1), hl
-	
-	ld	l,4 (ix)
-	ld	h,5 (ix)
+
+	ld	l, 4 (ix)
+	ld	h, 5 (ix)
 	; copy of the string start pointer:
 	ld (drawstringz_local_as + 2), hl
 
@@ -60,42 +60,42 @@ _drawstringz::
 	ld (drawstringz_local_i + 1), a
 
 rowloop:
-						   
-	exx 
-drawstringz_local_bd: ; start bc´ from destination base pointer
-	ld bc, #1234 ; stored as self-modifying code
+
 	exx
-	
+drawstringz_local_bd: ; start bc´ from destination base pointer
+	ld bc, #0x1234 ; stored as self-modifying code
+	exx
+
 drawstringz_local_as: ; start ix from string start
 	ld ix, #0x1234  ; stored as self-modifying code
-												
+
 	; get glyph data pattern number
 	exx
 	push de
 	exx
 	pop hl
-	ld	c,(ix) ; c is our glyph
-	ld	b,#0x00
+	ld	c, (ix) ; c is our glyph
+	ld	b, #0x00
 	add	hl, bc
-	ld	a,(hl)
-	ld	b,a
+	ld	a, (hl)
+	ld	b, a
 
 	; if it's the first pattern, we can skip actually drawing it because it's empty
 	or	a, a
-	jr	Z,fast_emptydata
+	jr	Z, fast_emptydata
 
-	; get the pre-shifted ghyph data from pattern index                       
-	ld  l,a
-	ld	h,#0x00
+	; get the pre-shifted ghyph data from pattern index
+	ld  l, a
+	ld	h, #0x00
 	add	hl, hl
-	ex	de,hl        
-	ld	hl,#(_propfont + 0x034e)
-	add	hl,de
-	ld	a,(hl)
-	
+	ex	de, hl
+	ld	hl, #(_propfont + 0x034e)
+	add	hl, de
+	ld	a, (hl)
+
 	; this is the first thing to be plotted, no or:ing needed
 	ld  hl, (drawstringz_local_bd + 1)
-	ld	(hl),a
+	ld	(hl), a
 
 fast_emptydata:
 
@@ -109,7 +109,7 @@ fast_emptydata:
 charloop:
 	inc	ix               ; next char
 	ld	a, (ix)
-	ld	e, a	
+	ld	e, a
 	or	a, a
 	jp	Z, endofstring   ; if zero, we're at end of string
 
@@ -118,7 +118,7 @@ charloop:
 	ld	d,  #0x00
 	add	hl, de
 	ld	a,  (hl)
-						
+
 	; get the glyph pattern number
 	push de
 	exx
@@ -127,51 +127,48 @@ charloop:
 	push hl
 	exx
 	pop hl
-	;ld  d, a      ; d = width
 	ld	l, (hl)   ; l = index
-	
-	; increment pixel offset 
-	;ld	a, c ; c is pixel offset
-	;add	a, d
-	add a, c
-	ld	b, a ; store pixel offset in b temporarily
 
-	; if glyph pattern is 0, skip drawing                        
+	; increment pixel offset
+	add a, c ; a = width, c = pixel offset
+	ld	b, a ; store new pixel offset in b temporarily
+
+	; if glyph pattern is 0, skip drawing
 	ld	a, l
 	or	a, a
-	jr	Z,emptydata
+	jr	Z, emptydata
 
-	; get the actual pixel data for the pattern, pre-shifted                        
-	ld	h,#0x00
+	; get the actual pixel data for the pattern, pre-shifted
+	ld	h,  #0x00
 	add	hl, hl
-	ex	de,hl
-	ld	l,c ; old pixel offset
-	ld	h,#0x00
+	ex	de, hl
+	ld	l,  c ; old pixel offset
+	ld	h,  #0x00
 	add	hl, hl
 	add	hl, de
 	ex	de, hl
 	exx
-	ld	a,(bc)
+	ld	a, (bc)
 	exx
-	ld	c, a
-	ld	hl,#(_propfont + 0x034e)
-	add	hl,de
+	ld	c,  a
+	ld	hl, #(_propfont + 0x034e)
+	add	hl, de
 
-	; "or" the pixels to screen                        
-	ld	a,(hl)
+	; "or" the pixels to screen
+	ld	a, (hl)
 	or	a, c
 	exx
-	ld	(bc),a
+	ld	(bc), a
 	exx
 
-	ld	c,b ; set the new pixel offset
-	; if pixel offset goes over byte boundary, plot the second pixel too
+	ld	c, b ; set the new pixel offset
+
 	bit 3, b
-	jr Z, withinbyte
-							
+	jp Z, charloop ; if pixel offset goes over byte boundary, plot the second pixel too
+
 	exx
-	inc bc	; move forward in screen 						
-	ld	a,(bc) ; get current pixels
+	inc bc	    ; move forward in screen
+	ld	a, (bc) ; get current pixels
 	exx
 	ld	c, a
 	inc	de
@@ -180,26 +177,21 @@ charloop:
 	ld	a, (hl)
 	or	a, c
 	exx
-	ld	(bc),a ; plot
+	ld	(bc), a ; plot pixels back
 	exx
-	; keep pixel offset within a byte                        
-	ld	a,b
-	and	a, #0x07
-	ld	c,a
-	jr	withinbyte
-emptydata:
-	
-	ld	c,b ; new pixel offset                            
-	bit 3, b  ; check if we moved to next byte
-	jr Z, withinbyte                        
-	ld	a,c     ; new byte, keep pixel offset within it
-	and	a, #0x07
-	ld	c,a                       
-	exx
-	inc	bc ; move to next byte
-	exx
-withinbyte:
+	; keep pixel offset within a byte
+    ld  c, b
+    res 3, c
+    jp  charloop
 
+emptydata:
+	ld	c, b     ; new pixel offset
+	bit 3, b     ; check if we moved to next byte
+	jp  Z, charloop
+	res 3, c ; new byte, keep pixel offset within it
+	exx
+	inc	bc       ; move to next byte
+	exx
 	jp	charloop
 
 endofstring:
@@ -217,7 +209,7 @@ endofstring:
 	; (we're within 8 pixels so simple +256 is enough)
 	ld hl, #drawstringz_local_bd + 2
 	inc (hl)
-	
+
 	; was this the last scanline?
 
 drawstringz_local_i:
@@ -225,6 +217,6 @@ drawstringz_local_i:
 	dec	a
 	ld (drawstringz_local_i + 1),a
 	jp	NZ,rowloop
-	
+
 	pop	ix
 	ret
