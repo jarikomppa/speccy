05e20c64c23a4e1cfb170fa9f02ac6f9d8308cec
jarikomppa <jari.komppa@gmail.com>
2018-12-08 16:12:36 +0200

    building ingame. Still a lot to do.

--- engarde/ingame.c
+++ engarde/ingame.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
-void update_info(unsigned char selected, unsigned char hand[5], unsigned char pos, unsigned char oldselected)
+unsigned char p_hand[5];
+unsigned char o_hand[5];
+
+void update_info(unsigned char selected, unsigned char pos, unsigned char oldselected)
 {
     char temp[40];
     char numtemp[3];
@@ -18,13 +21,13 @@ void update_info(unsigned char selected, unsigned char hand[5], unsigned char po
     {
         if (selected & (1 << i))
         {
-            if (gCardTypes[hand[i]].mFlags & CARDFLAG_FOCUS) focus = 1;
-            if (gCardTypes[hand[i]].mFlags & CARDFLAG_LEAP) leap = 1;
-            attack += gCardTypes[hand[i]].mAttack;
-            defend += gCardTypes[hand[i]].mDefend;
+            if (gCardTypes[p_hand[i]].mFlags & CARDFLAG_FOCUS) focus = 1;
+            if (gCardTypes[p_hand[i]].mFlags & CARDFLAG_LEAP) leap = 1;
+            attack += gCardTypes[p_hand[i]].mAttack;
+            defend += gCardTypes[p_hand[i]].mDefend;
             count++;
         }
-        if (gCardTypes[hand[i]].mFlags & CARDFLAG_FATIGUE)
+        if (gCardTypes[p_hand[i]].mFlags & CARDFLAG_FATIGUE)
             restcount--;
     }
 
@@ -126,7 +129,7 @@ void update_info(unsigned char selected, unsigned char hand[5], unsigned char po
 		{
 			temp[0] = 0;    
 			strcat(temp, "Card: ");
-			strcat(temp, gCardTypes[hand[pos]].mName);
+			strcat(temp, gCardTypes[p_hand[pos]].mName);
 			drawstringz(temp, 2, 14);
 		}
 	}	
@@ -145,14 +148,105 @@ char o_discardtop;
 #define P_ADDCARD(card) p_deck[p_decktop++] = (card) 
 #define P_GETCARD() p_deck[--p_decktop]
 #define P_DISCARD(card) p_discard[p_discardtop++] = (card)
+#define P_GETDISCARD() p_discard[--p_discardtop]
 
 #define O_ADDCARD(card) o_deck[o_decktop++] = (card) 
 #define O_GETCARD() o_deck[--o_decktop]
 #define O_DISCARD(card) o_discard[o_discardtop++] = (card)
+#define O_GETDISCARD() o_discard[--o_discardtop]
 
-void shuffle()
+void opponentdeck()
+{
+	char i, c7, c5, c4, c3, c1;
+	
+	switch (game_stage)
+	{
+		default:
+		case 0:
+			c7 = CARD_IDLE;
+			c5 = CARD_DEF1;
+			c4 = CARD_ATK1;
+			c3 = CARD_FOCUS;
+			c1 = CARD_ATK2;
+			break;
+		case 1:
+			c7 = CARD_DEF1;
+			c5 = CARD_ATK1;
+			c4 = CARD_FOCUS;
+			c3 = CARD_DEF2;
+			c1 = CARD_ATK2;
+			break;
+		case 2:
+			c7 = CARD_ATK1;
+			c5 = CARD_DEF1;
+			c4 = CARD_FOCUS;
+			c3 = CARD_ATK2;
+			c1 = CARD_DEF2;
+			break;
+		case 3:
+			c7 = CARD_ATK1;
+			c5 = CARD_DEF1;
+			c4 = CARD_ATK2;
+			c3 = CARD_FOCUS;
+			c1 = CARD_DEF2;
+			break;
+		case 4:
+			c7 = CARD_ATK1;
+			c5 = CARD_ATK2;
+			c4 = CARD_DEF1;
+			c3 = CARD_DEF2;
+			c1 = CARD_ATK1DEF1;
+			break;
+		case 5:
+			c7 = CARD_ATK1;
+			c5 = CARD_ATK2;
+			c4 = CARD_DEF1;
+			c3 = CARD_DEF2;
+			c1 = CARD_ATK1DEF2;
+			break;
+		case 6:
+			c7 = CARD_ATK2;
+			c5 = CARD_DEF2;
+			c4 = CARD_DEF1;
+			c3 = CARD_ATK3;
+			c1 = CARD_ATK2DEF1;
+			break;
+		case 7:
+			c7 = CARD_ATK2;
+			c5 = CARD_ATK3;
+			c4 = CARD_ATK1DEF2;
+			c3 = CARD_DEF2;
+			c1 = CARD_ATK2DEF1;
+			break;
+		case 8:
+			c7 = CARD_ATK2;
+			c5 = CARD_ATK1DEF2;
+			c4 = CARD_DEF2;
+			c3 = CARD_ATK2DEF1;
+			c1 = CARD_DEF3;
+			break;
+		case 9:
+			c7 = CARD_ATK3;
+			c5 = CARD_ATK1DEF2;
+			c4 = CARD_ATK2DEF1;
+			c3 = CARD_DEF3;
+			c1 = CARD_DEF3;
+			break;
+	}
+	for (i = 0; i < 7; i++) O_ADDCARD(c7);
+	for (i = 0; i < 5; i++) O_ADDCARD(c5);
+	for (i = 0; i < 4; i++) O_ADDCARD(c4);
+	for (i = 0; i < 3; i++) O_ADDCARD(c3);
+    O_ADDCARD(c1);
+}
+
+
+void player_shuffle()
 {
 	unsigned char i;
+	while (p_discardtop)
+		P_ADDCARD(P_GETDISCARD());
+
 	for (i = 0; i < p_decktop; i++)
 	{
 		char c1, c2;
@@ -165,7 +259,14 @@ void shuffle()
 			p_deck[c1] = p_deck[c2];
 			p_deck[c2] = t;
 		}
-	}
+	}	
+}
+
+void opponent_shuffle()
+{
+	unsigned char i;
+	while (o_discardtop)
+		O_ADDCARD(O_GETDISCARD());
 
 	for (i = 0; i < o_decktop; i++)
 	{
@@ -179,7 +280,38 @@ void shuffle()
 			o_deck[c1] = o_deck[c2];
 			o_deck[c2] = t;
 		}
+	}	
+}
+
+char focus()
+{
+	return 0b11111; // cards to keep
+}
+
+char resolve(unsigned char selected)
+{
+	char player_focus = selected;
+	unsigned char c;
+	
+	selected = 0b11111;
+	if (player_focus)
+		selected = focus();
+	for (c = 0; c < 5; c++)
+		if (selected & (1 << c))
+			P_DISCARD(p_hand[c]);
+	for (c = 0; c < 5; c++)
+	{
+		if (p_decktop == 0)
+			player_shuffle();
+		p_hand[c] = P_GETCARD();
+		if (o_decktop == 0)
+			opponent_shuffle();
+		o_hand[c] = O_GETCARD();
 	}
+
+	return 0; // no game over
+//	return -1; // player won
+//	return -2; // player lost
 }
 
 void ingame()
@@ -193,21 +325,22 @@ void ingame()
     unsigned char selected = 0;
     unsigned char commit = 0;
     unsigned char c;
-    unsigned char hand[5];
 	p_decktop = 0;
 	p_discardtop = 0;
 	o_decktop = 0;
 	o_discardtop = 0;
+	opponentdeck();
 	for (c = 0; c < 20; c++)
 		P_ADDCARD(playerdeck[c]);
 	for (c = 0; c < player_hurt; c++)
 		P_ADDCARD(CARD_HURT);
-	shuffle();
-    hand[0] = P_GETCARD();
-    hand[1] = P_GETCARD();
-    hand[2] = P_GETCARD();
-    hand[3] = P_GETCARD();
-    hand[4] = P_GETCARD();
+	player_shuffle();
+	opponent_shuffle();
+	for (c = 0; c < 5; c++)
+	{
+		p_hand[c] = P_GETCARD();
+		o_hand[c] = O_GETCARD();
+	}
     pos = 0;
     frame = 0;
 
@@ -227,28 +360,50 @@ void ingame()
 
         drawstringz("Dealing..", 2, 9);
 
+#if 0
         drawcard(CARD_BACK,1,1,COLOR(0,0,0,7));
         cardfx();
-        drawcard(hand[0],1,17,COLOR(0,0,0,7));
+        drawcard(p_hand[0],1,17,COLOR(0,0,0,7));
         cardfx();
         drawcard(CARD_BACK,6,1,COLOR(0,0,0,7));
         cardfx();
-        drawcard(hand[1],6,17,COLOR(0,0,0,7));
+        drawcard(p_hand[1],6,17,COLOR(0,0,0,7));
         cardfx();
         drawcard(CARD_BACK,11,1,COLOR(0,0,0,7));
         cardfx();
-        drawcard(hand[2],11,17,COLOR(0,0,0,7));
+        drawcard(p_hand[2],11,17,COLOR(0,0,0,7));
         cardfx();
         drawcard(CARD_BACK,16,1,COLOR(0,0,0,7));
         cardfx();
-        drawcard(hand[3],16,17,COLOR(0,0,0,7));
+        drawcard(p_hand[3],16,17,COLOR(0,0,0,7));
         cardfx();
         drawcard(CARD_BACK,21,1,COLOR(0,0,0,7));
         cardfx();
-        drawcard(hand[4],21,17,COLOR(0,0,0,7));
+        drawcard(p_hand[4],21,17,COLOR(0,0,0,7));
         cardfx();
-        
-        update_info(0,hand,0, 100);
+#else
+        drawcard(o_hand[0],1,1,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(p_hand[0],1,17,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(o_hand[1],6,1,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(p_hand[1],6,17,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(o_hand[2],11,1,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(p_hand[2],11,17,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(o_hand[3],16,1,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(p_hand[3],16,17,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(o_hand[4],21,1,COLOR(0,0,0,7));
+        cardfx();
+        drawcard(p_hand[4],21,17,COLOR(0,0,0,7));
+        cardfx();
+#endif
+        update_info(0,0, 100);
        
         while (1)
         {
@@ -265,7 +420,7 @@ void ingame()
 					triggered = 1; 
 					if (pos != 5) 
 					{
-						if (!(gCardTypes[hand[pos]].mFlags & CARDFLAG_IDLE))
+						if (!(gCardTypes[p_hand[pos]].mFlags & CARDFLAG_IDLE))
 							selected ^= 1 << pos; 
 					}
 					else 
@@ -279,17 +434,29 @@ void ingame()
                     c = COLOR(0,0,0,7);
                     switch (oldpos)
                     {
-                        case 0: if (selected &  1) c = COLOR(0,1,0,7); drawcard(hand[0], 1,17,c); break;
-                        case 1: if (selected &  2) c = COLOR(0,1,0,7); drawcard(hand[1], 6,17,c); break;
-                        case 2: if (selected &  4) c = COLOR(0,1,0,7); drawcard(hand[2],11,17,c); break;
-                        case 3: if (selected &  8) c = COLOR(0,1,0,7); drawcard(hand[3],16,17,c); break;
-                        case 4: if (selected & 16) c = COLOR(0,1,0,7); drawcard(hand[4],21,17,c); break;
+                        case 0: if (selected &  1) c = COLOR(0,1,0,7); drawcard(p_hand[0], 1,17,c); break;
+                        case 1: if (selected &  2) c = COLOR(0,1,0,7); drawcard(p_hand[1], 6,17,c); break;
+                        case 2: if (selected &  4) c = COLOR(0,1,0,7); drawcard(p_hand[2],11,17,c); break;
+                        case 3: if (selected &  8) c = COLOR(0,1,0,7); drawcard(p_hand[3],16,17,c); break;
+                        case 4: if (selected & 16) c = COLOR(0,1,0,7); drawcard(p_hand[4],21,17,c); break;
                         case 5: drawmug(6,26,17); break;
                     }
                     if (!commit)
                     {
-                        update_info(selected,hand,pos,oldselected);
+                        update_info(selected,pos,oldselected);
                     }
+					else
+					{
+						signed char result = resolve(selected);
+						commit = 0;
+						selected = 0;
+						if (result == -1)
+							game_state = 0;
+						if (result == -2)
+							game_state = 0;
+						if (result < 0)
+							return;
+					}						
                 }
             }
             frame++;
